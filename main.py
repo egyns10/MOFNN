66c48e860954cf90f3f944840b5002d4450eca3e
egyns10 <egyns10@nottingham.ac.uk>
2025-05-07 22:02:45 +0100

    Simplified code to debug

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 # main.py
-from preprocess import processCSVFile, removeDuplicatesFromColumns, cleanData
-from processCSV import load_csv_to_array, readCSV
+from preprocess import removeDuplicatesFromColumns, cleanData, readCSV
 from featurise import featuriseArray
 from randomForest import doRandomForest
 from linearReg import doLinearReg
 from gradBoost import doGradBoost
-from trainTestData import splitData
+#from trainTestData import splitData
 #from ttd import splitData2
 
 '''
@@ -14,7 +13,7 @@ filePath = "/Users/nso/Local Items/Uni/Y3/BEng Project/csv files/mof_crystallogr
 result = processCSVFile(filePath)
 print("Processed CSV Data:")
 for row in result[:5]:
-    print(row)
+    print(row) 
 
 result = removeDuplicatesFromColumns(result)
 print("\nData after removing duplicates from columns:\n")
@@ -37,23 +36,25 @@ clean = load_csv_to_array(csv_file)
 print("Original 2D Array:")
 print(clean)
 '''
-csv_file = 'mof5.csv'
+csv_file = "/Users/nso/Local Items/Uni/Y3/BEng Project/csv files/mof5.csv"
 clean = readCSV(csv_file)
+print(clean)
+print("done")
 
 featurisedData = featuriseArray(clean)
 print("\nfeaturised Data:")
 print(featurisedData)
 
 feature_columns = [1, 2]
-X_train, y_train, X_test, y_test = splitData(100, 100, feature_columns=feature_columns)
+#X_train, y_train, X_test, y_test = splitData(100, 100, feature_columns=feature_columns)
 
 #header, X_train, y_train, X_test, y_test = splitData2(featurisedData)
 
-rf_mse, rf_r2 = doRandomForest(X_train, y_train, X_test, y_test)
+rf_mse, rf_r2 = doRandomForest(featurisedData)
 print(f"\nRandom Forest Regressor - MSE: {rf_mse:.4f}, R²: {rf_r2:.4f}")
 
-gb_mse, gb_r2 = doGradBoost(X_train, y_train, X_test, y_test)
+gb_mse, gb_r2 = doGradBoost(featurisedData)
 print(f"\nGradient Boosting - MSE: {gb_mse:.4f}, R²: {gb_r2:.4f}")
 
 print("\nLinear Regression and Visualization:")
-doLinearReg(X_train, y_train, X_test, y_test)
+doLinearReg(featurisedData)
