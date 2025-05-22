8ab9c0725cef61d07919b64e1ce23e784cca3d5b
egyns10 <egyns10@nottingham.ac.uk>
2025-05-22 03:33:03 +0100

    Added data collection code

--- getData.py
+++ getData.py
@@ -1,12 +1,34 @@
 #getData.py
+import pandas as pd
 
-def createGrid(headers):
-    size = len(headers)
-    grid = [['' for _ in range(size + 1)] for _ in range(size + 1)]
-    for i in range(size):
-        grid[0][i + 1] = headers[i]
-        grid[i + 1][0] = headers[i]
-    return grid
+def createGrid(features,names,results):
+    #features: user chosen features e.g. PV, VF
+    #names: types of algorithms e.g. XG_RF
+    #results: R2, MSE etc.
+    #^ these are all pd.df
+
+    #flatten features into one single string
+    title = ', '.join(features.values.flatten().astype(str))
+    
+    resultName = results.iloc[:, 0].tolist()
+    namesML = names.iloc[:, 0].tolist()
+    
+    #empty grid
+    grid = [[None for _ in range(len(resultName) + 1)] for _ in range(len(namesML) + 1)]
+    
+    #[0,0]
+    grid[0][0] = title
+
+    #first row
+    for j, rows in enumerate(resultName):
+        grid[0][j + 1] = rows
+
+    #first column
+    for i, cols in enumerate(namesML):
+        grid[i + 1][0] = cols
+
+    df = pd.DataFrame(grid)
+    return df
 
 def intoArray(array,i,j,n):     
     #if arrayMSE[i+1,j+1] != "x":
--- main.py
+++ main.py
@@ -7,10 +7,11 @@ from randomForest import doRandomForest, randomTreeXGBoost
 from linearReg import doLinearReg
 from gradBoost import doGradBoost
 from getData import createGrid, intoArray
-from validate import csvValidate, columnChoose
+from validate import csvValidate, columnChoose, UGorUV
 
 #------------------------------------
 
+#read in all properties from file
 filepath = 'h2_capacity_gcmc.csv'
 propertiesReadFile = readCSV(filepath)
 propertiesNoDup = removeDup(propertiesReadFile)
@@ -20,91 +21,78 @@ propertiesIsolated = isolateCols(propertiesClean,0,6)
 csvValidate(propertiesClean)
 print(propertiesIsolated[:2])
 
+#read in H2 capacities from same file
 gcmcReadFile = readCSV(filepath)
 gcmcNoDup = removeDup(gcmcReadFile)
 gcmcClean = cleanData(gcmcNoDup)
 gcmcUGIsolated = isolateCols(gcmcClean,7,"null")
 gcmcUVIsolated = isolateCols(gcmcClean,8,"null")
-
 print(gcmcUGIsolated[:2])
 
+
+#choose properties to use + validate
+features, max = columnChoose(propertiesClean)
+#returns the features chosen (as pd.df) and the number of features chosen
+
+#user chooses on UG or UV
+trueValue = UGorUV(gcmcUGIsolated,gcmcUVIsolated)
+
+#setup for easy data collection
+#TODO: Change this if the calculated accuracy values or the no. of algorithms are altered!
+namesML = ['SK_RF','XG_RF','SK_GB','SK_LR']
+dfNamesML = pd.DataFrame(data=namesML)
+namesAccuracy = ['MSE','R²']
+dfNamesAccuracy = pd.DataFrame(data=namesAccuracy)
+collectedData = createGrid(features,dfNamesML, dfNamesAccuracy)
+#print(collectedData)
+
+
 '''
+
+
+
+
+
+#grid = [['' for _ in range(lengthData+1)] for _ in range(lengthData+1)]
+#gridHeaders = ["Density","GSA","VSA","Void Fraction","Pore Volume","Largest Cavity Diameter","Pore Limiting Diameter"]
+#print(grid)
+
 gb_mse, gb_r2 = doGradBoost(propertiesClean.iloc[:,[1,2]],gcmcUGIsolated)
 #gb_mse, gb_r2 = doGradBoost(propertiesClean[1],propertiesClean[2],gcmcUG)
 print(f"\nScikit-learn | Gradient Boosting - MSE: {gb_mse:.4f}, R²: {gb_r2:.4f}")
-'''
-lengthData = propertiesClean.shape[1]
-#choose properties to use + validate
-features, max = columnChoose(propertiesClean)
-
-'''
-grid = [['' for _ in range(lengthData+1)] for _ in range(lengthData+1)]
-gridHeaders = ["Density","GSA","VSA","Void Fraction","Pore Volume","Largest Cavity Diameter","Pore Limiting Diameter"]
-
-SKRFMSE = createGrid(gridHeaders)
-SKRFR2  = createGrid(gridHeaders)
-XGRFMSE = createGrid(gridHeaders)
-XGRFR2 = createGrid(gridHeaders)
-SKBGMSE = createGrid(gridHeaders)
-SKBGR2 = createGrid(gridHeaders)
-SKLRMSE = createGrid(gridHeaders)
-SKLRR2 = createGrid(gridHeaders)
 
 
-for i in range(1,lengthData+1):
-    for j in range(1,lengthData+1):
+dataIsolated = isolateCols(propertiesClean ,features)
 
-        dataIsolated = isolateCols(propertiesClean ,i-1, j-1)
+rf_mse, rf_r2 = doRandomForest(dataIsolated, trueValue)
+print(f"\nScikit-learn | Random Forest Regressor - MSE: {rf_mse:.4f}, R²: {rf_r2:.4f}")
 
-        rf_mse, rf_r2 = doRandomForest(dataIsolated)
-        print(f"\nScikit-learn | Random Forest Regressor - MSE: {rf_mse:.4f}, R²: {rf_r2:.4f}")
-
-        SKRFMSE[i][j] = rf_mse
-        SKRFR2[i][j] = rf_r2
+SKRFMSE = rf_mse
+SKRFR2 = rf_r2
 
 
-        rfxg_mse, rfxg_r2 = randomTreeXGBoost(dataIsolated)
-        print(f"\nXGBoost | Random Forest Regressor - MSE: {rfxg_mse:.4f}, R²: {rfxg_r2:.4f}") 
+rfxg_mse, rfxg_r2 = randomTreeXGBoost(dataIsolated, trueValue)
+print(f"\nXGBoost | Random Forest Regressor - MSE: {rfxg_mse:.4f}, R²: {rfxg_r2:.4f}") 
 
-        XGRFMSE[i][j] = rfxg_mse
-        XGRFR2[i][j] = rfxg_r2
+XGRFMSE[i][j] = rfxg_mse
+XGRFR2[i][j] = rfxg_r2
 
-        gb_mse, gb_r2 = doGradBoost(dataIsolated)
-        print(f"\nScikit-learn | Gradient Boosting - MSE: {gb_mse:.4f}, R²: {gb_r2:.4f}")
+gb_mse, gb_r2 = doGradBoost(dataIsolated, trueValue)
+print(f"\nScikit-learn | Gradient Boosting - MSE: {gb_mse:.4f}, R²: {gb_r2:.4f}")
 
-        SKBGMSE[i][j] = gb_mse
-        SKBGR2[i][j] = gb_r2
+SKBGMSE[i][j] = gb_mse
+SKBGR2[i][j] = gb_r2
 
-        lr_mse, lr_r2 = doLinearReg(dataIsolated,gridHeaders[i],gridHeaders[j])
-        print(f"\nScikit-learn | Linear Regression = MSE {lr_mse:.4f},R²: {lr_r2:.4f}")
-        print("\nScikit-learn | Linear Regression and Visualization:")
+lr_mse, lr_r2 = doLinearReg(dataIsolated, trueValue, gridHeaders[i], gridHeaders[j])
+print(f"\nScikit-learn | Linear Regression = MSE {lr_mse:.4f},R²: {lr_r2:.4f}")
+print("\nScikit-learn | Linear Regression and Visualization:")
 
-        SKLRMSE[i][j] = lr_mse
-        SKLRR2[i][j] = lr_r2
+SKLRMSE[i][j] = lr_mse
+SKLRR2[i][j] = lr_r2
 
-        print(f"Done\nCurrent i: {i} and Current j: {j}")
+print(f"Done\nCurrent i: {i} and Current j: {j}")
 
-df1 = pd.DataFrame(SKRFMSE)
-saveAsCSV(df1, '/Users/nso/Desktop/SKRFMSE.csv')
 
-df2 = pd.DataFrame(SKRFR2)
-saveAsCSV(df2, '/Users/nso/Desktop/SKRFR2.csv')
-
-df3 = pd.DataFrame(XGRFMSE)
-saveAsCSV(df3, '/Users/nso/Desktop/XGRFMSE.csv')
-
-df4 = pd.DataFrame(XGRFR2)
-saveAsCSV(df4, '/Users/nso/Desktop/XGRFR2.csv')
-
-df5 = pd.DataFrame(SKBGMSE)
-saveAsCSV(df5, '/Users/nso/Desktop/SKBGMSE.csv')
-
-df6 = pd.DataFrame(SKBGR2)
-saveAsCSV(df6, '/Users/nso/Desktop/SKBGR2.csv')
-
-df7 = pd.DataFrame(SKLRMSE)
-saveAsCSV(df7, '/Users/nso/Desktop/SKLRMSE.csv')
-
-df8 = pd.DataFrame(SKLRR2)
-saveAsCSV(df8, '/Users/nso/Desktop/SKLRR2.csv')
+#df1 = pd.DataFrame(SKRFMSE)
+#saveAsCSV(df1, '/Users/nso/Desktop/SKRFMSE.csv')
 '''
